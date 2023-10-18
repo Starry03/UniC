074f6c4e1a6ae9ce585e63e7b2d801d4fa6051da
Starry03 <andrea.starrantino@gmail.com>
2023-10-18 23:00:51 +0200

    Clean up main.c + .gitignore

--- main.c
+++ main.c
@@ -2,22 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Prova{
-    char c;
-    int n;
-}Prova_;
-typedef Prova_ *Prova;
 int main() {
-    Prova p = (Prova) malloc(sizeof(Prova_));
-    p->n = 1;
 
-    Dictionary dict = Dictionary_Create(10);
-    Dictionary_Add(
-            dict,
-            p,
-            p,
-            INT
-    );
-    Prova res = Dictionary_Get(dict, p, INT);
-    Dictionary_Dealloc(dict);
 }
