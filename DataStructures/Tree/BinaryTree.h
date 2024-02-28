fdd9f8c27982e7dc3367cc93717d86ba1a328d1f
Starry03 <andrea.starrantino#gmail.com>
2024-02-28 19:54:57 +0100

    indent

--- DataStructures/Tree/BinaryTree.h
+++ DataStructures/Tree/BinaryTree.h
@@ -8,26 +8,22 @@
 #include <stdbool.h>
 #include "../../Utils/types.h"
 
-typedef struct BinaryTreeNode {
-    Generic info;
-    struct BinaryTreeNode *left;
-    struct BinaryTreeNode *right;
+typedef struct BinaryTreeNode
+{
+	Generic info;
+	struct BinaryTreeNode *left;
+	struct BinaryTreeNode *right;
 } BinaryTreeNode;
 
 typedef BinaryTreeNode *BinaryTree;
 
 #define TREE_NODE_SIZE 24
 
-
 BinaryTree EmptyTree();
 
 BinaryTree InitTreeNode();
 
-BinaryTree NewTree(
-        Generic info,
-        BinaryTree left,
-        BinaryTree right
-);
+BinaryTree NewTree(Generic info, BinaryTree left, BinaryTree right);
 
 BinaryTree BinaryTree_Left(BinaryTree tree);
 
@@ -43,7 +39,7 @@ bool BinaryTree_IsInBinarySearchTree(BinaryTree tree, Generic value);
 
 void BinaryTree_SetLeft(BinaryTree tree, BinaryTree left);
 
-void BinaryTree_SetDeepLeft(BinaryTree* tree, BinaryTree left);
+void BinaryTree_SetDeepLeft(BinaryTree *tree, BinaryTree left);
 
 bool IsLeaf(BinaryTree tree);
 
@@ -51,4 +47,4 @@ bool HasTwoNodes(BinaryTree tree);
 
 bool BinaryTree_DeleteTreeNode(BinaryTree *tree, Generic value);
 
-#endif //BINARYTREE_H
+#endif // BINARYTREE_H
