2b16f354e3d417ef1041b65b30803beceb03ba7c
Starry03 <andrea.starrantino#gmail.com>
2024-02-28 20:27:12 +0100

    magic

--- Utils/Hashing/HashFunctions.c
+++ Utils/Hashing/HashFunctions.c
@@ -5,7 +5,7 @@
 #include "HashFunctions.h"
 #include <stdint.h>
 
-#define PRIME 31
+#define MAGIC 0xdeadbeef
 
 size_t normalize(size_t val, size_t size) {
     return val % size;
@@ -23,7 +23,7 @@ size_t hash_addr(Generic addr, size_t size)
 {
 	size_t addr_ = (size_t)addr;
 	addr_ = addr_ ^ (addr_ >> 4);
-	addr_ = (addr_ + (addr_ << 5)) ^ 0xdeadbeef;
+	addr_ = (addr_ + (addr_ << 5)) ^ MAGIC;
 	addr_ = addr_ ^ (addr_ >> 11);
 	return normalize(addr_, size);
 }
