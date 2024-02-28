646de0e25fed8d1b0b8fe3cf76300acc5d555f26
Starry03 <andrea.starrantino#gmail.com>
2024-02-28 21:33:43 +0100

    Update Dict_Init size and fix type casting in alloc function

--- main.c
+++ main.c
@@ -6,17 +6,17 @@
 void alloc(Dict d)
 {
 	static int i = 0;
-	Dict_Add(d, i++, 10, NULL, NULL);
+	Dict_Add(d, (Generic)i, (Generic)10, NULL, NULL);
+	i++;
 }
 
 int main() {
-	Dict dict = Dict_Init(10);
+	Dict dict = Dict_Init(100);
 	int i = 0;
-	while (i++ < 10)
+	while (i++ < 100)
 		alloc(dict);
 	Dict_Status(dict);
 	int a = (int)Dict_Get(dict, 0);
 	printf("%d\n", a);
 	Dict_Free(dict);
 }
-
