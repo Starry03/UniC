f22b6d2f670779a8c327e540221f2378fde2f321
Starry03 <andrea.starrantino@gmail.com>
2024-04-28 01:45:29 +0200

    Remove unused code in main.c

--- main.c
+++ main.c
@@ -4,11 +4,5 @@
 
 int	main(int argc, char const *argv[])
 {
-	Matrix	matrix;
-
-	matrix = Matrix_Init(10, 10);
-	Matrix_LaplaceDet(matrix);
-	Matrix_Print(matrix);
-	Matrix_Free(matrix);
 	return (0);
 }
