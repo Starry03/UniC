6136fe2aee9c319beab976c0913e64262ba5227f
Starry03 <andrea.starrantino@gmail.com>
2024-11-07 18:02:21 +0100

    ADD: Implement uvector2 and complex data structures with initialization and memory management functions

--- algebra/vector/uvector2.c
+++ algebra/vector/uvector2.c
@@ -1,7 +1,7 @@
-#include "UVector2.h"
+#include "uvector2.h"
 #include <stdlib.h>
 
-t_uvector2	UVector2_init(uint64_t x, uint64_t y)
+t_uvector2	uvector2_init(uint64_t x, uint64_t y)
 {
 	t_uvector2	vector;
 
@@ -12,7 +12,7 @@ t_uvector2	UVector2_init(uint64_t x, uint64_t y)
 	vector->y = y;
 	return (vector);
 }
-void	UVector2_free(t_uvector2 vector)
+void	uvector2_free(t_uvector2 vector)
 {
 	if (!vector)
 		return ;
--- algebra/vector/uvector2.h
+++ algebra/vector/uvector2.h
@@ -11,7 +11,7 @@ typedef struct s_uvector2
 
 typedef t_uvector2_	*t_uvector2;
 
-t_uvector2			UVector2_init(uint64_t x, uint64_t y);
-void				UVector2_free(t_uvector2 vector);
+t_uvector2			uvector2_init(uint64_t x, uint64_t y);
+void				uvector2_free(t_uvector2 vector);
 
 #endif
