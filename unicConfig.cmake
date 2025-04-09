d670896b1ecdab14ba4c1d0f6d762973139ddda1
Starry03 <andrea.starrantino@gmail.com>
2025-04-09 23:27:40 +0200

    fuck cmake

--- unicConfig.cmake
+++
@@ -1,4 +0,0 @@
-get_filename_component(UNIC_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-set(UNIC_INCLUDE_DIRS "${UNIC_CMAKE_DIR}/../include")
-set(UNIC_LIBRARY_DIR "${UNIC_CMAKE_DIR}/../lib")
-set(UNIC_LIBRARIES "${UNIC_LIBRARY_DIR}/unic.a")
